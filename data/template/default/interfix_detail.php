49eff98648c47cd3896c39f4ccc8fcf7e19cb3d4
sl321123@126.com <githubsl830101>
2018-04-11 17:45:17 +0800

    解决 艺术节相关 详情页中的图片超出区域问题

--- data/template/default/interfix_detail.php
+++ data/template/default/interfix_detail.php
@@ -39,7 +39,7 @@
 			<div class="maincen2">
 				<div id="weizhi">所在位 &gt; <?=x6cms_location($category,' > ');?></div>
 
-				<div class="padding-20-65">
+				<div class="">
 					<h1 class="text-align-center"><?=$detail['title']?></h1>
 					<?=$detail['content']?>
 				</div>
